ff46098c8904a1a5f32cb144ca60c3f49c018e80
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-08-10 14:13:28 +0100

    test complete

--- src/v2/groups/controllers/createGroup.ts
+++ src/v2/groups/controllers/createGroup.ts
@@ -5,7 +5,6 @@ import { prisma } from '../../../lib/prisma';
 import {
    existingResourceError,
    forbiddenError,
-   notFoundError,
    internalServerError
 } from '../../../lib/errorMessages';
 import { validatePermissions } from '../../../lib/util';
